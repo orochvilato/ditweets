26a49c66005e80f7dac725254d55dedf04a48d9b
Olivier ROCH VILATO <olivier.rochvilato@chilbp.fr>
2018-05-17 08:04:29 -0400

    debug

--- ditweets/jobs/test.py
+++ ditweets/jobs/test.py
@@ -1,6 +1,6 @@
 definition = dict(
     name = __name__,
-    active = False,
+    active = True,
     type = 'cron',
     params = dict(minute='*/30'),
     description = "Test",
--- run.py
+++ run.py
@@ -1,8 +1,10 @@
 # -*- coding: utf-8 -*-
-from ditweets import app
+from ditweets import app,jobs
 
 
 
 
 if __name__ == "__main__":
+    jobs.stop_scheduler()
+
     app.run(port=8889,debug=True,processes=1)
