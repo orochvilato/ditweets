6f25b4cff5494f691317673945f67704a84cc49f
Olivier ROCH VILATO <olivier.rochvilato@chilbp.fr>
2018-06-25 13:15:52 -0400

    followers

--- ditweets/views/user.py
+++ ditweets/views/user.py
@@ -217,9 +217,14 @@ def tops():
     pipeline = [{'$match':{'error':None}},
                 {'$group':pgroup},
                 {'$sort':{'n':-1}}]
-    html = "<html><body><table border='1'><thead><tr><td>Utilisateur</td><td>Followers</td><td>RT + Like</td></tr></thead><tbody>"
+    accounts = []
     for t in mdb.logs.aggregate(pipeline):
-        html += "<tr><td>{user}</td><td>{f}</td><td>{n}</td></tr>".format(f=followers.get(t['_id']['user'],0),user=t['_id']['user'],n=t['n'])
+        accounts.append(dict(f=followers.get(t['_id']['user'],0),user=t['_id']['user'],n=t['n']))
+    accounts.sort(key=lambda x:x['f']*x['n'],reverse=True)
+
+    html = "<html><body><table border='1'><thead><tr><td>Utilisateur</td><td>Followers</td><td>RT + Like</td><td>Impact</td></tr></thead><tbody>"
+    for a in accounts:
+        html += "<tr><td>{user}</td><td>{f}</td><td>{n}</td><td>{i}</td></tr>".format(user=a['user'],f=a['f'],n=a['n'],i=a['f']*a['n'])
 
     if 0:
         html += "</tbody></table>"
